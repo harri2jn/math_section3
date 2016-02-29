a2e38a44bd0e0aa25250505bf7865438ae5221b2
Morgan Benton <morgan.benton@gmail.com>
2016-02-29 08:51:08 -0500

    added tests/code for +-*/

--- spec/math_spec.js
+++ spec/math_spec.js
@@ -2,7 +2,21 @@
 var math = require('../math');
 
 describe("A program that does basic arithmetic", function() {
+
   it("can add two numbers together", function() {
     expect(math.add(3,5)).toBe(8);
   });
+
+  it("can subtract two numbers", function() {
+    expect(math.subtract(3,5)).toBe(-2);
+  });
+
+  it("can multipy two numbers together", function() {
+    expect(math.multipy(3,5)).toBe(15);
+  });
+
+  it("can divide two numbers", function() {
+    expect(math.divide(15,5)).toBe(3);
+  });
+
 });
